a6305612c065e832cc3621d9b25cf8ba3811b3f0
birhman <birhmanharish@gmail.com>
2024-11-29 12:53:25 +0530

    Delete users directory

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.user.username}'s profile"
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import profile_view, edit_profile
-
-app_name = 'users'
-
-urlpatterns = [
-    path('profile/', profile_view, name='profile'),
-    path('edit-profile/', edit_profile, name='edit_profile'),
-]
